0892aa884acebe071c5acf48c244f3ad47212bd5
Marcelo Lopes <mblopesfilho@bol.com.br>
2022-05-12 13:56:23 -0400

    tópico - métodos especiais

--- Exemplos/3 - Métodos Especiais/src/Caneta.java
+++ Exemplos/3 - Métodos Especiais/src/Caneta.java
@@ -1,14 +1,29 @@
 public class Caneta {
+    
+    // Atributos
+    
     public String modelo;
     private float ponta;
     private String cor;
     private boolean tampada;
+
+    // Método Construtor
 
     public Caneta() { //Este é o método construtor.
         this.tampar();
         this.cor = "Azul";
     }
 
+    // Métodos Públicos
+
+    public void status(){
+        System.out.println("Sobre a caneta:");
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta: " + this.getPonta());
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Tampada: " + this.getTampada());
+    }
+    
     public void tampar(){
         this.tampada = true;
     }
@@ -16,6 +31,18 @@ public class Caneta {
     public void destampar(){
         this.tampada = false;
     }
+
+    public void rabiscar(){
+        if (this.tampada == true){
+            System.out.println("ERRO! Não posso rabiscar");
+        }
+
+        else{
+            System.out.println("Posso rabiscar!");
+        }
+    }
+
+    // Métodos Especiais
 
     public boolean getTampada(){
         return this.tampada;
@@ -39,23 +66,5 @@ public class Caneta {
 
     public void setPonta(float p){
         this.ponta = p;
-    }
-
-    public void rabiscar(){
-        if (this.tampada == true){
-            System.out.println("ERRO! Não posso rabiscar");
-        }
-
-        else{
-            System.out.println("Posso rabiscar!");
-        }
-    }
-
-    public void status(){
-        System.out.println("Sobre a caneta:");
-        System.out.println("Modelo: " + this.getModelo());
-        System.out.println("Ponta: " + this.getPonta());
-        System.out.println("Cor: " + this.cor);
-        System.out.println("Tampada: " + this.getTampada());
     }
 }
--- Exemplos/3 - Métodos Especiais/src/Principal.java
+++ Exemplos/3 - Métodos Especiais/src/Principal.java
@@ -1,7 +1,6 @@
-public class App {
+public class Principal {
     public static void main(String[] args) {
         Caneta c1 = new Caneta();
-        
         c1.setModelo("BIC");
         //c1.modelo = "BIC"; apesar de chamar pelo método poderia ser chamado também pelo atributo diretamente pois o mesmo é público. OBS: Se for chamado por ambos a prioridade é da chamada direto no atributo.
 
@@ -16,6 +15,6 @@ public class App {
         
         //System.out.println("Tenho uma caneta " + c1.modelo + " de ponta " + c1.getPonta());
 
-        //Com a utilização do método constructor, é possível a criação de objetos com atributos pré determinados que assim que chamados mostrarão valor pré estabelicidos e não "Null" e que podem, ou não, serem modificados mediante uso de métodos get e set de acordo com a necessidade do programador. Nesse projeto é possível ver isso nos atributos cor e tampada.
+        //Com a utilização do método constructor, é possível a criação de objetos com atributos pré determinados que assim que chamados mostrarão valor pré estabelicidos e não "Null" e que podem, ou não, serem modificados mediante uso de métodos get e set de acordo com a necessidade do programador e das permissões do código. Nesse projeto é possível ver isso nos atributos cor e tampada.
     }   
 }
